ffa72168a0d2c89c5fe7d09549950f3d5d98d2bf
Omid Asadi <omiddzedd@gmail.com>
2024-03-09 08:32:28 +0000

    changed logo size

--- deFi-city/src/Components/NavBar.tsx
+++ deFi-city/src/Components/NavBar.tsx
@@ -5,7 +5,7 @@ export const Navbar = ({ isDarkMode }: NavBarProps) => {
 	return (
 		<div className={`flex justify-center ${isDarkMode ? 'bg-[#192633]' : 'bg-[#E5D9CC]'}`}>
 			<img
-				className='w-24'
+				className='w-28'
 				src={`${isDarkMode ? '/defi.png' : '/defi2.png'}`}
 				alt='Logo'
 			/>
