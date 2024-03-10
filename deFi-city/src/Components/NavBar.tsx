f2d7274a68385ccfc071a0b9af90533930295661
Omid Asadi <omiddzedd@gmail.com>
2024-03-10 11:39:48 +0000

    added credits for coingecko

--- deFi-city/src/Components/NavBar.tsx
+++ deFi-city/src/Components/NavBar.tsx
@@ -4,6 +4,15 @@ type NavBarProps = {
 export const Navbar = ({ isDarkMode }: NavBarProps) => {
 	return (
 		<div className={`flex justify-center ${isDarkMode ? 'bg-[#192633]' : 'bg-[#E5D9CC]'}`}>
+			<div className='absolute top-10 left-14  w-28 flex gap-2'>
+				<span className={`${isDarkMode ? 'text-white' : 'text-black'} whitespace-nowrap`}>Powered by</span>
+				<img
+					src={`${isDarkMode ? '/coingecko_logo_white.png ' : '/coingecko_logo_dark.png'}`}
+					alt='CoinGecko Logo'
+					style={{ width: '120px' }}
+				/>
+			</div>
+
 			<img
 				className='w-28'
 				src={`${isDarkMode ? '/defi.png' : '/defi2.png'}`}
